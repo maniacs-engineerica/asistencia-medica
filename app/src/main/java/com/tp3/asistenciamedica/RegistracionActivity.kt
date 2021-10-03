168462301c7d7dae660220d84a87f969b131ac03
Mati Cohen <mcohen@encircleapp.com>
2021-10-03 14:05:50 -0300

    - Trabajando en página de inicio

--- app/src/main/java/com/tp3/asistenciamedica/RegistracionActivity.kt
+++ app/src/main/java/com/tp3/asistenciamedica/RegistracionActivity.kt
@@ -3,9 +3,9 @@ package com.tp3.asistenciamedica
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class RegistrationActivity : AppCompatActivity() {
+class RegistracionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        setContentView(R.layout.activity_registracion)
     }
 }
