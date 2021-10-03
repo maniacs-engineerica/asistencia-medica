890cbb265d2ca44b56b8c9b2e4b3eec5c9354cad
Mati Cohen <mcohen@encircleapp.com>
2021-10-03 00:26:54 -0300

    - Pantalla de login terminada - Trabajando en pantalla de registración

---
+++ app/src/main/java/com/tp3/asistenciamedica/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.tp3.asistenciamedica
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
