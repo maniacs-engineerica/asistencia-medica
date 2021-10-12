878206c14f85026af840cc1d86860533c4a41e57
Mati Cohen <mcohen@encircleapp.com>
2021-10-12 10:07:10 -0300

    - Estilos textedit agregados en la generación de turnos

--- app/src/main/java/com/tp3/asistenciamedica/ui/estudios/EstudiosFragment.kt
+++ app/src/main/java/com/tp3/asistenciamedica/ui/estudios/EstudiosFragment.kt
@@ -1,16 +1,13 @@
 package com.tp3.asistenciamedica.ui.estudios
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.tp3.asistenciamedica.EstudiosAdapter
+import com.tp3.asistenciamedica.adapters.EstudiosAdapter
 import com.tp3.asistenciamedica.databinding.FragmentEstudiosBinding
 
 class EstudiosFragment : Fragment() {
