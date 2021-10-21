ceb7cbb9dad6a97253a5360d4bbdffa1b157f2a5
Ezequiel Girola <ezequielgirola@gmail.com>
2021-10-21 10:44:04 -0300

    Turnos generation
    - Added zonedTime to entity Turno
    - Added logic generation in Fragment

--- app/src/main/java/com/tp3/asistenciamedica/entities/Turno.kt
+++ app/src/main/java/com/tp3/asistenciamedica/entities/Turno.kt
@@ -1,4 +1,16 @@
 package com.tp3.asistenciamedica.entities
 
+import java.time.ZonedDateTime
+
 class Turno(val profesional: String) {
+
+    public lateinit var dateTime: ZonedDateTime
+
+    constructor() : this("")
+
+    public fun withDate(date: ZonedDateTime): Turno {
+        this.dateTime = date
+        return this
+    }
+
 }
--- app/src/main/java/com/tp3/asistenciamedica/ui/generacion_turnos/GeneradorTurnoFragment.kt
+++ app/src/main/java/com/tp3/asistenciamedica/ui/generacion_turnos/GeneradorTurnoFragment.kt
@@ -1,15 +1,28 @@
 package com.tp3.asistenciamedica.ui.generacion_turnos
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.ktx.Firebase
 import com.tp3.asistenciamedica.databinding.FragmentGeneradorTurnosBinding
+import com.tp3.asistenciamedica.entities.Turno
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.math.truncate
 
 
 class GeneradorTurnoFragment : Fragment() {
+
+    private val db = Firebase.firestore
+
 
     companion object {
         fun newInstance() = GeneradorTurnoFragment()
@@ -31,10 +44,78 @@ class GeneradorTurnoFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(GeneradorTurnoViewModel::class.java)
 
+        binding.submitTurnos.setOnClickListener {
+            generateTurnos()
+        }
+
 
         return root
     }
 
+
+    @SuppressLint("NewApi")
+    private fun generateTurnos() {
+        // TODO: Replace logic with the correct API code
+
+        // Assumes that here we are with valid data
+        val date: Editable = binding.fechaAGenerar.text
+        val especial: Editable = binding.especialidad.text
+
+        val duration: Editable = binding.duracion.text
+        val horaFinal: Editable = binding.horaFinal.text
+        val horaInicial: Editable = binding.horaInicial.text
+        val separation: Editable = binding.separacion.text
+
+        val parsedDate: LocalDate = LocalDate.parse(
+            date,
+            DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy")
+        )
+
+
+        val parsedHoraFinal: Double = Integer.valueOf(horaFinal.toString().replace(":", "")).toDouble()
+        var parsedHoraInicial: Double = Integer.valueOf(horaFinal.toString().replace(":", "")).toDouble()
+
+        while (parsedHoraInicial < parsedHoraFinal ) {
+
+            val dateTime: ZonedDateTime = parsedDate.atStartOfDay(ZoneId.of("America/Argentina/Buenos_Aires"))
+
+            dateTime.plusHours(
+                truncate((parsedHoraInicial / 100)).toLong()
+            )
+
+            dateTime.plusMinutes(
+                (parsedHoraInicial - truncate((parsedHoraInicial / 100)) * 100).toLong()
+            )
+
+            val turno: Turno = Turno("someProfesional").withDate(dateTime)
+
+
+            generateTurno(turno)
+
+
+            val parsedDuration = Integer.valueOf(duration.toString())
+            val parsedSeparation = Integer.valueOf(separation.toString())
+
+
+            parsedHoraInicial += (parsedDuration + parsedSeparation)
+
+        }
+
+
+
+    }
+
+
+    private fun generateTurno(turno: Turno) {
+        db.collection("turnos")
+            .add(turno)
+            .addOnSuccessListener { documentReference ->
+            }
+            .addOnFailureListener { e ->
+            }
+
+
+    }
 
 
     override fun onDestroyView() {
